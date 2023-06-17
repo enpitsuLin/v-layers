8d3086fbb909def2f7b023756e9a4aa255c8f341
enpitsulin <enpitsulin@gmail.com>
2023-06-17 23:16:48 +0800

    feat: index page

---
+++ playground/src/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <router-link to="/accessible">
+      Accessible Map
+    </router-link>
+    <p>(accessible.vue)</p>
+    <p> Example of an accessible map. </p>
+  </div>
+  <div>
+    <router-link to="/center">
+      Advanced View Positioning
+    </router-link>
+    <p>(center.vue)</p>
+    <p>
+      This example demonstrates how a map's view can be adjusted so a geometry or coordinate is positioned at a specific
+      pixel location.
+    </p>
+  </div>
+</template>
