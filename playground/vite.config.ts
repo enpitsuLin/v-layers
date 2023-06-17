9e6db48b0fbf621126a42b9391353fb0d3412967
enpitsulin <enpitsulin@gmail.com>
2023-06-17 15:53:59 +0800

    chore: remove isCustomElement option

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,12 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    template: {
-      // 添加以下内容
-      compilerOptions: {
-        isCustomElement: tag => tag.startsWith('Ol')
-      }
-    }
-  })],
+  plugins: [vue()],
 })
