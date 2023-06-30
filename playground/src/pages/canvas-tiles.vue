3aef4b55c86219df3bb2ea8e6181f5b56373f783
enpitsulin <enpitsulin@gmail.com>
2023-06-30 14:52:32 +0800

    feat: add example

---
+++ playground/src/pages/canvas-tiles.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import 'ol/ol.css'
+
+import { VLayer, VMap, VSource } from '@v-layers/components'
+import { View } from 'ol'
+
+const view = new View({
+  center: [0, 0],
+  zoom: 1,
+})
+</script>
+
+<template>
+  <VMap :view="view" style="height:400px;width:100%">
+    <VLayer.Tile>
+      <VSource.OSM />
+    </VLayer.Tile>
+    <VLayer.Tile>
+      <VSource.TileDebug />
+    </VLayer.Tile>
+  </VMap>
+</template>
